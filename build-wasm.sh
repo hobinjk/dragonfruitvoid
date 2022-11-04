325b75417f3c5ee5546cfe56afd8bc3268857f67
James Hobin <hobinjk@gmail.com>
2022-11-04 13:27:11 -0400

    Copy index.html in build-wasm

--- build-wasm.sh
+++ build-wasm.sh
@@ -4,3 +4,4 @@ set -ex
 cargo build --profile wasm-release --target wasm32-unknown-unknown
 wasm-bindgen --out-name dragonfruitvoid --out-dir dist --target web target/wasm32-unknown-unknown/wasm-release/dragonfruitvoid.wasm
 rm -r dist/assets && cp -r assets dist/assets
+cp index.html dist/
