23d59802a4a28d8e81b71b3d99b22a72fa9860ba
James Hobin <hobinjk@gmail.com>
2022-11-04 12:22:26 -0400

    Add the world's laziest build script

---
+++ build-wasm.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+set -ex
+
+cargo build --profile wasm-release --target wasm32-unknown-unknown
+wasm-bindgen --out-name dragonfruitvoid --out-dir dist --target web target/wasm32-unknown-unknown/wasm-release/dragonfruitvoid.wasm
+rm -r dist/assets && cp -r assets dist/assets
