10f86140b2fdcb70ba49219583c017592d05964f
James Hobin <hobinjk@gmail.com>
2024-11-16 21:51:37 -0500

    Slightly nerf spew radius

--- src/aoes.rs
+++ src/aoes.rs
@@ -12,7 +12,7 @@ pub const AOE_BASE_COLOR: Color = Color::rgba(0.9, 0.9, 0., 0.4);
 pub const AOE_DETONATION_COLOR: Color = Color::rgba(0.7, 0., 0., 0.7);
 
 pub const SPEW_DAMAGE: f32 = 40.;
-pub const SPEW_RADIUS: f32 = 220. * GAME_TO_PX;
+pub const SPEW_RADIUS: f32 = 200. * GAME_TO_PX;
 const SPEW_SPACING: f32 = 30. * GAME_TO_PX;
 const SPEW_DYDX: f32 = -0.3;
 
