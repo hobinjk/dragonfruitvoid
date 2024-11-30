ece9d464937a86f628734ef5468b831809860aed
James Hobin <hobinjk@gmail.com>
2024-11-30 13:54:04 -0500

    Allow Text to wrap hints instead of doing it manually

--- src/hints.rs
+++ src/hints.rs
@@ -17,10 +17,7 @@ const HINTS_ALL_PHASES: [HintTiming; 1] = [HintTiming {
     start: 0.,
     hint: "Welcome to The Dragonfruitvoid!
 
-Move with WASD and shoot with left mouse
-button. Your skills are 4: Pull, E: Blink,
-V: Dodge, Space: Jump, and
-R: Portal (advanced).",
+Move with WASD and shoot with left mouse button. Your skills are 4: Pull, E: Blink, V: Dodge, Space: Jump, and R: Portal (advanced).",
 }];
 
 const HINT_INDIVIDUAL_PHASE: HintTiming = HintTiming {
@@ -34,33 +31,22 @@ fn phase_hints(phase: &GameState) -> Vec<HintTiming> {
         GameState::PurificationOne => {
             vec![HintTiming {
                 start: 1.,
-                hint: "Push the white orb through the green
-targets with your bullets! Prevent the
-crabs from reaching the orbs by killing
-them (also with your bullets). The black
-circles are THE VOID and will damage you.",
+                hint: "Push the white orb through the green targets with your bullets! Prevent the crabs from reaching the orbs by killing them (also with your bullets). The black circles are THE VOID and will damage you.",
             }]
         }
         GameState::Jormag => {
             vec![
                 HintTiming {
                     start: 1.,
-                    hint: "The big red circle is a boss. Pew pew
-it to victory!",
+                    hint: "The big red circle is a boss. Pew pew it to victory!",
                 },
                 HintTiming {
                     start: 5.5,
-                    hint: "Red puddles will follow you for a short
-time before they drop on the ground and
-become a damaging zone. Move quickly to
-drop them out of the way. Dodge out to
-avoid taking too much damage.",
+                    hint: "Red puddles will follow you for a short time before they drop on the ground and become a damaging zone. Move quickly to drop them out of the way. Dodge out to avoid taking too much damage.",
                 },
                 HintTiming {
                     start: 15.5,
-                    hint: "Green circles need to be soaked. Pick one
-of the three to stand in before it
-explodes.",
+                    hint: "Green circles need to be soaked. Pick one of the three to stand in before it explodes.",
                 },
             ]
         }
@@ -73,69 +59,56 @@ explodes.",
         GameState::Kralkatorrik => {
             vec![HintTiming {
                 start: 5.5,
-                hint: "Space is limited in this phase. Watch
-out for the lines of VOID.",
+                hint: "Space is limited in this phase. Watch out for the lines of VOID.",
             }]
         }
         GameState::PurificationTwo => {
             vec![
                 HintTiming {
                     start: 1.,
-                    hint: "Push the orb to the targets again. Watch
-out, the timecaster is a nasty enemy who
-needs to be removed or they'll push away
-the orb.",
+                    hint: "Push the orb to the targets again. Watch out, the timecaster is a nasty enemy who needs to be removed or they'll push away the orb.",
                 },
                 HintTiming {
                     start: 4.,
-                    hint: "The orb shoots bees out now, you
-know how it is with bees.",
+                    hint: "The orb shoots bees out now, you know how it is with bees.",
                 },
             ]
         }
         GameState::Mordremoth => {
             vec![HintTiming {
                 start: 21.,
-                hint: "Shockwaves incoming! These blue waves
-need to be dodged, jumped over, or blinked
-through to prevent massive damage.",
+                hint: "Shockwaves incoming! These blue waves need to be dodged, jumped over, or blinked through to prevent massive damage.",
             }]
         }
         GameState::Zhaitan => {
             vec![HintTiming {
                 start: 5.5,
-                hint: "Noodles attack the area near them.
-Kill them before they kill you!",
+                hint: "Noodles attack the area near them. Kill them before they kill you!",
             }]
         }
         GameState::PurificationThree => {
             vec![HintTiming {
                 start: 1.,
-                hint: "Push the orb to the targets. There is
-now a big saltspray dragon. Do what you do
-to dragons, you heartless monster.",
+                hint: "Push the orb to the targets. There is now a big saltspray dragon. Do what you do to dragons, you heartless monster.",
             }]
         }
         GameState::SooWonOne => {
             vec![HintTiming {
                 start: 1.,
-                hint: "Get ready for everything from every
-phase all at once!",
+                hint: "Get ready for everything from every phase all at once!",
             }]
         }
         GameState::PurificationFour => {
             vec![HintTiming {
                 start: 1.,
-                hint: "Time to get revenge on the orb by
-killing it! Beware, your bullets still push.",
+                hint:
+                    "Time to get revenge on the orb by killing it! Beware, your bullets still push.",
             }]
         }
         GameState::SooWonTwo => {
             vec![HintTiming {
                 start: 1.,
-                hint: "Everything from every phase all at
-once part two: Electric Boogaloo. Don't
-let the Obliterator or Goliath hit you!",
+                hint: "Everything from every phase all at once part two: Electric Boogaloo. Don't let the Obliterator or Goliath hit you!",
             }]
         }
         GameState::Nothing => {
