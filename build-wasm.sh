a4313f967755b2ca3eec2aa5611e46c02b16a42e
James Hobin <hobinjk@gmail.com>
2024-11-30 23:58:12 -0500

    Add wasm-opt pass

--- build-wasm.sh
+++ build-wasm.sh
@@ -3,5 +3,6 @@ set -ex
 
 cargo build --profile wasm-release --target wasm32-unknown-unknown
 wasm-bindgen --out-name dragonfruitvoid --out-dir dist --target web target/wasm32-unknown-unknown/wasm-release/dragonfruitvoid.wasm
+wasm-opt -Oz --output dist/dragonfruitvoid_bg.wasm dist/dragonfruitvoid_bg.wasm
 rm -r dist/assets && cp -r assets dist/assets
 cp index.html dist/
